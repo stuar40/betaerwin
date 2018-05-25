30769867fcac0dee9c8d1fb403fd558137e94962
erwinnav <erwin310@gmail.com>
2018-05-24 21:56:46 -0600

    config

--- config.js
+++ config.js
@@ -1,3 +1,16 @@
+/*const mysql = require('mysql');
+
+const connection = mysql.createConnection(
+    {
+		 host: '160.153.16.62',
+		  user: 'autoparking2018',
+		  password: 'autoparking@2018',
+		  database: 'autoparking_v1'
+    }
+)
+module.exports = connection;
+*/
+
 
 var mysql = require('mysql');
 
@@ -11,23 +24,3 @@ var mysqlPool  = mysql.createPool({
 });
 
 module.exports = mysqlPool;
-
-//*/
-/*
-const connection = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'autoparking',
-        password: 'autoparking',
-        database: 'autoparking'
-    });
-  connection.connect(function(err){
-  if(!err) {
-      console.log("Database is connected");
-  } else {
-      console.log("Error while connecting with database");
-  }
-  });
-*/
-
-module.exports = connection;
